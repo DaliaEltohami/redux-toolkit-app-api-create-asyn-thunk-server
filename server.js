baa3ccdabd1acfa132eb36b0236904840f1b584d
dalia eltohami <daliaeltohami1991@gmail.com>
2024-04-19 03:18:52 +0200

    setup env file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.write("hiiiiiiiiii");
+  res.send("hiiiiiiiiii");
 });
 
 app.post("/api/users", (req, res) => {
@@ -17,7 +17,9 @@ app.post("/api/users", (req, res) => {
   }, 1500);
 });
 
-const PORT = process.env.PORT || "5000";
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server Running on Port 5000");
 });
+
+console.log(process.env.PORT);
